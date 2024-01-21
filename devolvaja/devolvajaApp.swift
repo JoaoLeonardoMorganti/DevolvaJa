171633515ff50736c07a4e34b32bfff545722119
João Leonardo Andrade Morganti Silva <joaoleonardo.dev@gmail.com>
2024-01-21 15:54:07 -0300

    Create SwiftUI project

---
+++ devolvaja/devolvajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  devolvajaApp.swift
+//  devolvaja
+//
+//  Created by Joao Leonardo Morganti on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct devolvajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
